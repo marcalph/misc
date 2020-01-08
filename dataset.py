b1939f17340b7ff737715f441ceab3772a12c5ae
marcalph <marcalph@protonmail.com>
2020-01-08 16:22:26 +0100

    fix image dataset building
    Signed-off-by: marcalph <marcalph@protonmail.com>

--- dataset.py
+++ dataset.py
@@ -5,19 +5,21 @@
 from datetime import datetime, timedelta
 from google_images_download import google_images_download
 
+bulks = [100, 300]
+for b in bulks:
+    response = google_images_download.googleimagesdownload()
+    arguments = {"keywords": "chien",
+                "limit": b,
+                "chromedriver": "/opt/chromedriver-79.0.3945.36/chromedriver",
+    #             "time_range": '{"time_min":"02/01/2018", "time_max":"03/01/2018"}', # MM/DD/YYYY
+                "print_urls": True,
+                "output_directory": "./downloads/buld"+str(b),
+                "no_directory": True}
+    paths = response.download(arguments)
 
-# response = google_images_download.googleimagesdownload()
-# arguments = {"keywords": "chien",
-#             "limit": 200,
-#             "time_range": '{"time_min":"02/01/2018", "time_max":"03/01/2018"}', # MM/DD/YYYY
-#             "print_urls": True, 
-#             "output_directory": "./downloads/test02",
-#             "no_directory": True}  
-# paths = response.download(arguments)
 
-
-start = "01/01/2018"
-for i in range(11):
-    end = str(int(start[:2])+1).zfill(2)+start[2:]
-    print(f"{i}th sample has range {start=} - {end=}")
-    start = end
+# start = "01/01/2018"
+# for i in range(11):
+#     end = str(int(start[:2])+1).zfill(2)+start[2:]
+#     print(f"{i}th sample has range {start=} - {end=}")
+#     start = end
