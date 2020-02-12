aec7cc598f3be87621d019e99f6f9510b0fb750c
marcalph <marcalph@protonmail.com>
2020-02-12 10:31:44 +0100

    change viz function
    Signed-off-by: marcalph <marcalph@protonmail.com>

--- viz.py
+++ viz.py
@@ -3,6 +3,10 @@ from matplotlib import cm
 
 
 def dual_plot(img1, img2):
+    """
+    simple side by side plot
+    useful to inspect image transform
+    """
     fig = plt.figure()
     fig.add_subplot(1,2, 1)
     plt.imshow(img1)
@@ -13,16 +17,20 @@ def dual_plot(img1, img2):
 
 
 
-def square_plot(img_seq, square, train_labels=None):
+def batch_plot(img_batch, label_batch=None, side=5):
+    """
+    viz function to inspect image data
+    displays `side`**2 images of a batch by default
+    """
     fig = plt.figure(figsize=(2*square, 2*square))
-    for i, img in enumerate(img_seq):
+    for i, img in enumerate(img_batch):
         plt.subplot(square, square, i+1)
         plt.yticks([])
         plt.xticks([])
         plt.grid(False)
         plt.imshow(img, cmap=plt.cm.binary)
         if train_labels:
-            plt.xlabel(train_labels[i])
+            plt.xlabel(label_batch[i])
     plt.show()
     return None
 
@@ -34,4 +42,4 @@ if __name__ == '__main__':
     import numpy as np
     img = np.random.random((50, 50))
     dual_plot(img, img.T)
-    square_plot([img]*9, 3, ["white_noise"]*9)
+    batch_plot([img]*9, 3, ["white_noise"]*9)
