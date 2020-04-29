c1d37b8656d5b72753dca3aa0ba21a3d7e015af9
marcalph <marcalph@protonmail.com>
2020-04-29 15:45:12 +0200

    add race condition

---
+++ src/race.py
@@ -0,0 +1,17 @@
+import concurrent.futures
+
+# global variable not thread safe
+counter = 0
+
+
+def increment_counter(fake_value):
+    global counter
+    for _ in range(100):
+        counter += 1
+
+
+if __name__ == "__main__":
+    fake_data = [x for x in range(1000000)]
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5000) as executor:
+        executor.map(increment_counter, fake_data)
+    print(counter)
