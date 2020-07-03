37230bfe84127b3107fd90afc7dc08e41d3cbbc3
marcalph <marcalph@protonmail.com>
2020-07-03 19:09:57 +0200

    clean fgsm attacks

--- img/attack.py
+++ img/attack.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-epsilons = [0, .05, .1, .15, .2, .25, .3]
+epsilons = [0, .15]
 pretrained_model = "data/MNIST/lenet_mnist_model.pth"
 use_cuda=False
 
@@ -44,15 +44,12 @@ device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "c
 
 # Initialize the network
 model = Net().to(device)
-
-# Load the pretrained model
 model.load_state_dict(torch.load(pretrained_model, map_location='cpu'))
-
-# Set the model in evaluation mode. In this case this is for the Dropout layers
+# set evaluation mode for dropout layers
 model.eval()
 
 
-# FGSM attack code
+# FGSM attack
 def fgsm_attack(image, epsilon, data_grad):
     # Collect the element-wise sign of the data gradient
     sign_data_grad = data_grad.sign()
@@ -64,22 +61,20 @@ def fgsm_attack(image, epsilon, data_grad):
     return perturbed_image
 
 
-def test(model, device, test_loader, epsilon):
-    # Accuracy counter
-    correct = 0
+def lbfgs_attack(image, target):
+    pass
+
+
+def test(model, device, test_loader, num_samples=5):
     adv_examples = []
-    # Loop over all examples in test set
-    for data, target in test_loader:
-        # Send the data and label to the device
+    for i in range(num_samples):
+        data, target = next(test_loader)
         data, target = data.to(device), target.to(device)
-        # Set requires_grad attribute of tensor. Important for Attack
+        # Set requires_grad attribute of tensor. Important for attacks
         data.requires_grad = True
-        # Forward pass the data through the model
         output = model(data)
         init_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
         # If the initial prediction is wrong, dont bother attacking, just move on
-        if init_pred.item() != target.item():
-            continue
         # Calculate the loss
         loss = F.nll_loss(output, target)
         # Zero all existing gradients
@@ -89,63 +84,35 @@ def test(model, device, test_loader, epsilon):
         # Collect datagrad
         data_grad = data.grad.data
         # Call FGSM Attack
-        perturbed_data = fgsm_attack(data, epsilon, data_grad)
+        perturbed_data = fgsm_attack(data, .15, data_grad)
         # Re-classify the perturbed image
         output = model(perturbed_data)
         # Check for success
-        final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-        if final_pred.item() == target.item():
-            correct += 1
-            # Special case for saving 0 epsilon examples
-            if (epsilon == 0) and (len(adv_examples) < 5):
-                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
-        else:
-            # Save some adv examples for visualization later
-            if len(adv_examples) < 5:
-                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
-    # Calculate final accuracy for this epsilon
-    final_acc = correct/float(len(test_loader))
-    print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
-    # Return the accuracy and an adversarial example
-    return final_acc, adv_examples
+        final_pred = output.max(1, keepdim=True)[1] # get the index of the max 
+        adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+        adv_examples.append((target.item(), init_pred.item(), final_pred.item(), data.squeeze().detach().numpy(), adv_ex))
+    return adv_examples
 
 
-
-accuracies = []
-examples = []
-
-# Run test for each epsilon
-for eps in epsilons:
-    acc, ex = test(model, device, test_loader, eps)
-    accuracies.append(acc)
-    examples.append(ex)
+test_loader = iter(test_loader)
+adv_examples = test(model, device, test_loader)
 
 
-
-plt.figure(figsize=(5,5))
-plt.plot(epsilons, accuracies, "*-")
-plt.yticks(np.arange(0, 1.1, step=0.1))
-plt.xticks(np.arange(0, .35, step=0.05))
-plt.title("Accuracy vs Epsilon")
-plt.xlabel("Epsilon")
-plt.ylabel("Accuracy")
-plt.show()
-
-# Plot several examples of adversarial samples at each epsilon
 cnt = 0
-plt.figure(figsize=(8,10))
-for i in range(len(epsilons)):
-    for j in range(len(examples[i])):
-        cnt += 1
-        plt.subplot(len(epsilons),len(examples[0]),cnt)
-        plt.xticks([], [])
-        plt.yticks([], [])
-        if j == 0:
-            plt.ylabel("Eps: {}".format(epsilons[i]), fontsize=14)
-        orig,adv,ex = examples[i][j]
-        plt.title("{} -> {}".format(orig, adv))
-        plt.imshow(ex, cmap="gray")
-plt.tight_layout()
+plt.figure(figsize=(9,9))
+num_samples = len(adv_examples)
+for i in range(num_samples):
+    print(i)
+    target, orig_pred, adv_pred, orig_data, adv_data = adv_examples[i]
+    plt.subplot(num_samples, 2, 2*i+1)
+    plt.xticks([], [])
+    plt.yticks([], [])
+    plt.ylabel("{}".format(adv_examples[i][0]), fontsize=14)
+    plt.imshow(orig_data, cmap="gray")
+    plt.title("{} -> {}".format(target, orig_pred))
+    plt.subplot(num_samples, 2, 2*i+2)
+    plt.xticks([], [])
+    plt.yticks([], [])
+    plt.imshow(adv_data, cmap="gray")
+    plt.title("{} -> {}".format(orig_pred, adv_pred))
 plt.show()
