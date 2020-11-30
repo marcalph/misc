0aae5eca8cf536666a48998fee62d262d422e0c6
marcalph <marcalph@protonmail.com>
2020-11-30 17:42:35 +0100

    audio base

---
+++ audio/audio.py
@@ -0,0 +1,19 @@
+# Uncomment the following line to run in Google Colab
+# !pip install torchaudio
+import torch
+import torchaudio
+import requests
+import matplotlib.pyplot as plt
+url = "https://pytorch.org/tutorials/_static/img/steam-train-whistle-daniel_simon-converted-from-mp3.wav"
+r = requests.get(url)
+with open('data/test.wav', 'wb') as f:
+    f.write(r.content)
+
+filename = "data/test.wav"
+waveform, sample_rate = torchaudio.load(filename)
+
+print("Shape of waveform: {}".format(waveform.size()))
+print("Sample rate of waveform: {}".format(sample_rate))
+
+plt.figure()
+plt.plot(waveform.t().numpy())
